09da39314380b6626307905c84b9d3d6978bd6b4
msnproduction <111676859+berthutapea@users.noreply.github.com>
2025-01-11 02:38:18 +0700

    fix: improve map

--- src/components/organisms/Map/index.jsx
+++ src/components/organisms/Map/index.jsx
@@ -1,41 +1,24 @@
 import React from 'react';
-import {
-  MapContainer,
-  TileLayer,
-  ZoomControl,
-  Marker,
-  Popup,
-} from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import './Map.css';
-
-import placeholderIcon from '../../../assets/images/icons/placeholder.png';
-
-const customIcon = new L.Icon({
-  iconUrl: placeholderIcon,
-  iconSize: [40, 40],
-});
-
-const position = [-6.318049403965516, 107.37920228561168];
 
 function Map() {
   return (
-    <MapContainer
-      className="map-action"
-      center={position}
-      zoom={15}
-      zoomControl={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <ZoomControl position="bottomright" />
-      <Marker position={position} icon={customIcon}>
-        <Popup>STTI Sony Sugema</Popup>
-      </Marker>
-    </MapContainer>
+    <div className="grid gap-6 mt-10">
+      <div className="col-span-12 md:col-span-8 lg:col-span-6 xl:col-span-6">
+        <div className="box border dark:border-gray-700 shadow-lg rounded-lg overflow-hidden">
+          <div className="box-body p-0">
+            <iframe
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4879.527440752902!2d107.37766074174469!3d-6.318219627431961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69772a3359107b%3A0x1f10a6889825470!2sSMK%20TEKNOLOGI%20KARAWANG!5e1!3m2!1sen!2sid!4v1736536960120!5m2!1sen!2sid"
+              width="100%"
+              height="400"
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              className="rounded-lg border-none"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
